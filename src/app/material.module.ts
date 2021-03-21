b8ae9a389d1b03058994c8e09cf5779b9cfdb495
Akib13 <akibahmed@iut-dhaka.edu>
2021-03-21 09:18:45 +0600

    #App_Commited_first_time
    It is just the design of the app.

---
+++ src/app/material.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+    imports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatTabsModule,
+        MatCardModule,
+        MatFormFieldModule
+    ],
+    exports: [
+        MatBadgeModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule,
+        MatTabsModule,
+        MatCardModule,
+        MatFormFieldModule
+    ]
+})
+
+export class MaterialModule { }
---
+++ src/app/weddings/traditional-weddings/traditional-weddings.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TraditionalWeddingsComponent } from './traditional-weddings.component';
+
+describe('TraditionalWeddingsComponent', () => {
+  let component: TraditionalWeddingsComponent;
+  let fixture: ComponentFixture<TraditionalWeddingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TraditionalWeddingsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TraditionalWeddingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
